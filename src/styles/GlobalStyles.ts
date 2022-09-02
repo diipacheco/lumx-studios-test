f96a7741f818bd5a34e34c2b892a7136e3f8156d
diipacheco <edilsonpacheco6@gmail.com>
2022-09-01 22:08:17 -0300

    Initialize theme structure

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    outline: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    max-width: 100vw;
+    max-height: 100vh;
+    width: 100%;
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Barlow', sans-serif;
+
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  :root {
+    /* Primary Colors */
+
+    --background: #14142B;
+    --dark-purple: #262338;
+    --light-purple: #4E4B66;
+    --dark-grey: #6E7191;
+    --grey: #D9DBE9;
+    --light-grey: #EFF0F6;
+    --text: #FCFCFC;
+    --white: #FFFFFF;
+
+  }
+
+
+   
+`;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    background: 'var(--background)',
+    elements: 'var(--dark-purple)',
+    text: 'var(--text)',
+  },
+};
