4ec98d5380a7227d569f70d531ede83058f943e0
diipacheco <edilsonpacheco6@gmail.com>
2022-09-01 22:23:58 -0300

    Adds more colors to the theme

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -5,5 +5,10 @@ export default {
     background: 'var(--background)',
     elements: 'var(--dark-purple)',
     text: 'var(--text)',
+    lightPurple: 'var(--light-purple)',
+    darkGrey: 'var(--dark-grey)',
+    grey: 'var(--grey)',
+    lightGrey: 'var(lightGrey)',
+    white: 'var(white)',
   },
 };
